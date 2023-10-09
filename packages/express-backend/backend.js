d7c620123286fd265be846b00ccc2246d36b739b
Russ Sobti <russ@recombinantsoftware.com>
2023-10-09 09:57:38 -0700

    Finished part 1 - Setting up a backend!

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,7 +42,21 @@ const findUserByName = (name) => {
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    /* Added my own stuff here */
+    const job = req.query.job;
+    if (job != undefined && name != undefined){
+        let result = findUserByName(name);
+        result = result.filter((user) => user['job'] === job);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else if (job != undefined){
+        let result = users['users_list']
+            .filter((user) => user['job'] === job);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else if (name != undefined){
         let result = findUserByName(name);
         result = {users_list: result};
         res.send(result);
@@ -52,6 +66,42 @@ app.get('/users', (req, res) => {
     }
 });
 
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
+
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
+/* Delete User Code */
+const deleteUser = (id) => {
+    const newUsers = users['users_list']
+        .filter((user) => user['id'] !== id);
+    users['users_list'] = newUsers;
+}
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    deleteUser(id);
+    res.send();
+});
 
 
 app.get('/', (req, res) => {
